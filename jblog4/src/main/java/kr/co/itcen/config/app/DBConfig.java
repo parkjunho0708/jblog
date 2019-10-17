f9f856b87cd31e0e5049dc412467e651f4bb3c0d
Junho Park <jhpark870708@daum.net>
2019-10-17 19:03:21 +0900

    DBConfig 접속방법 수정

--- jblog4/src/main/java/kr/co/itcen/config/app/DBConfig.java
+++ jblog4/src/main/java/kr/co/itcen/config/app/DBConfig.java
@@ -3,11 +3,10 @@ package kr.co.itcen.config.app;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,18 +16,49 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @PropertySource("classpath:kr/co/itcen/config/app/properties/jdbc.properties") // properties를 읽어주는 어노테이션 (classpath를 잡아줌.)
 public class DBConfig {
 	
-	@Autowired
-	private Environment env; // PropertyResolver
+//	@Autowired
+//	private Environment env; // PropertyResolver
+	
+	@Value(value = "${jdbc.driverClassName}")
+	private String driverClassName;
+	
+	@Value(value = "${jdbc.url}")
+	private String url;
+	
+	@Value(value = "${jdbc.username}")
+	private String userName;
+	
+	@Value(value = "${jdbc.password}")
+	private String password;
+	
+	@Value(value = "${jdbc.initialSize}")
+	private int initialSize;
+	
+	@Value(value = "${jdbc.maxActive}")
+	private int maxActive;
+	
+	
+//	@Bean
+//	public DataSource dataSource() {
+//		BasicDataSource basicDataSource = new BasicDataSource();
+//		basicDataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+//		basicDataSource.setUrl(env.getProperty("jdbc.url"));
+//		basicDataSource.setUsername(env.getProperty("jdbc.username"));
+//		basicDataSource.setPassword(env.getProperty("jdbc.password"));
+//		basicDataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
+//		basicDataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
+//		return basicDataSource;
+//	}
 	
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-		basicDataSource.setUrl(env.getProperty("jdbc.url"));
-		basicDataSource.setUsername(env.getProperty("jdbc.username"));
-		basicDataSource.setPassword(env.getProperty("jdbc.password"));
-		basicDataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
-		basicDataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
+		basicDataSource.setDriverClassName(driverClassName);
+		basicDataSource.setUrl(url);
+		basicDataSource.setUsername(userName);
+		basicDataSource.setPassword(password);
+		basicDataSource.setInitialSize(initialSize);
+		basicDataSource.setMaxActive(maxActive);
 		return basicDataSource;
 	}
 	
